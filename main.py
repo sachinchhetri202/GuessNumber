e9624b0e7828e9cd413c7350534258a23a1ca4cc
sachinchhetri202 <71232069+sachinchhetri202@users.noreply.github.com>
2020-10-28 17:58:13 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+
+number = random.randint(1, 9)
+guess = 0
+count = 0
+print('********  Welcome to Guessing Game.  ********\n')
+while guess != number and guess != "exit":
+    try:
+        guess = int(input("Guess the number between 1 to 9: "))
+
+        if guess == "exit":
+            break
+
+        count += 1  # adds each time you enter a wrong number
+
+        if count == 1 and guess == number:
+            print("Congratulations! You guessed the number in your first try.")
+        elif guess < number:
+            print("The number you entered is lower than the guessed number. Try Again.\n")
+        elif guess > number:
+            print("The number you entered is higher than the guessed number. Try Again.\n")
+        else:
+            print("You got it!")
+            print("And it took you", count, "tries to guess the number.")
+    except ValueError:
+        print("It looks like the number is missing.")
+
+
+
